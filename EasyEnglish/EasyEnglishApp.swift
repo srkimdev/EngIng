37300a3658df42f8b80037fdb1de3c7e36845779
srkimdev <emirated3858@gmail.com>
2024-09-10 20:50:20 +0900

    Initial Commit

---
+++ EasyEnglish/EasyEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyEnglishApp.swift
+//  EasyEnglish
+//
+//  Created by 김성률 on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EasyEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
