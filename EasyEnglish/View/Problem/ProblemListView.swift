a7888eceb9f3e171a9a441f1b3e60edf1dee1d28
srkimdev <emirated3858@gmail.com>
2024-09-12 21:07:31 +0900

    Add: ProblemListView

---
+++ EasyEnglish/View/Problem/ProblemListView.swift
@@ -0,0 +1,18 @@
+//
+//  ProblemListView.swift
+//  EasyEnglish
+//
+//  Created by 김성률 on 9/12/24.
+//
+
+import SwiftUI
+
+struct ProblemListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProblemListView()
+}
