72f61241d4863fbddb94a904c6bea2a10d7ab170
srkimdev <emirated3858@gmail.com>
2024-10-13 20:58:10 +0900

    Fix: delete screenWidth, screenHeight

--- EasyEnglish/Static/Constants.swift
+++ EasyEnglish/Static/Constants.swift
@@ -8,13 +8,6 @@
 import Foundation
 import SwiftUI
 
-struct Constants {
-    static let screenWidth = UIScreen.main.bounds.width
-    static let screenHeight = UIScreen.main.bounds.height
-    
-    
-}
-
 enum Colors {
     
     static let backgroundColor = Color(red: 0.9647058845, green: 0.9647058845, blue: 0.9647058845)
